beb8cdf9034f5bcb691c7300416c1cf5ba9d8abf
OTISDav <davidbotcholi2003@gmail.com>
2025-06-24 19:25:29 +0000

    integration CRUD (menu & reservation)

--- Models/ReservationDto.cs
+++ Models/ReservationDto.cs
@@ -4,6 +4,7 @@ namespace FrontendApp.Models
     {
         public int Id { get; set; }
         public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty; 
         public int MenuId { get; set; }
         public DateTime ReservationDate { get; set; }
         public string Status { get; set; } = string.Empty;
